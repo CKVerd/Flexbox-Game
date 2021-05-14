97528df429b016f7d9c7406ef9c2518f43046737
Cyverd <verdad.cyrilken@gmail.com>
2021-05-14 16:55:56 +0800

    final touches

--- views/js/main.js
+++ views/js/main.js
@@ -1,22 +1,61 @@
+var score = 0;
+var plateJust = 0;
+var plateAlign = 0;
+var cupJust = 0;
+var cupAlign = 0;
+
+function clearInformation() {
+    document.getElementById("inputJustify").value = "";
+    document.getElementById("inputAlign").value = "";
+    console.log("Cleared");
+}
+
+function getScore() {
+    var cup = document.querySelector(".gameCanvas");
+    if (plateJust == cupJust && plateAlign == cupAlign) {
+        score += 1
+        console.log(score)
+        randomize();
+        clearInformation();
+        cup.classList.remove('justify-flex-start');
+        cup.classList.remove('justify-flex-end');
+        cup.classList.remove('justify-flex-center');
+        cup.classList.remove('align-flex-start');
+        cup.classList.remove('align-flex-end');
+        cup.classList.remove('align-flex-center');
+        if (score == 1) {
+            alert("Good! You know have " + score + " point.")
+        } else {
+            alert("Good! You know have " + score + " points.")
+        }
+    } else {
+        alert("Game Over! Try Again.")
+        score = 0;
+    }
+}
 function randomize() {
     var plate = document.querySelector(".plate-location");
     var justify = Math.floor(Math.random() * (4 - 1) + 1); //The maximum is exclusive and the minimum is inclusive
     var align = Math.floor(Math.random() * (4 - 1) + 1);
-    
+
+
     if (justify == "1") {
         plate.classList.remove('justify-flex-end');
         plate.classList.remove('justify-flex-center')
         plate.classList.add('justify-flex-start');
+        plateJust = 1;
 
     } else if (justify == "2") {
         plate.classList.remove('justify-flex-start');
         plate.classList.remove('justify-flex-center')
         plate.classList.add('justify-flex-end');
+        plateJust = 2;
 
     } else if (justify == "3") {
         plate.classList.remove('justify-flex-start');
         plate.classList.remove('justify-flex-end')
         plate.classList.add('justify-flex-center');  
+        plateJust = 3;
     } else {
         plate.classList.remove('justify-flex-start');
         plate.classList.remove('justify-flex-end')
@@ -28,19 +67,19 @@ function randomize() {
         plate.classList.remove('align-flex-end');
         plate.classList.remove('align-flex-center');
         plate.classList.add('align-flex-start');
-        console.log("true-start")
+        plateAlign = 1;
 
     } else if (align == "2") {
         plate.classList.remove('align-flex-start');
         plate.classList.remove('align-flex-center');
         plate.classList.add('align-flex-end');
-        console.log("true-end")
+        plateAlign = 2;
 
     } else if (align == "3") {
         plate.classList.remove('align-flex-start');
-        plate.classList.remove('align-flex-end');
+        plate.classList.remove('align-flex-end');   
         plate.classList.add('align-flex-center');
-        console.log("true-center")
+        plateAlign = 3;
 
     } else {
         plate.classList.remove('align-flex-start');
@@ -52,6 +91,8 @@ function randomize() {
     if (justify == "1" && align == "1") {
         randomize()
     }
+
+    console.log(plateAlign, plateJust)
 }
 
 function refresh() {
@@ -64,18 +105,21 @@ function refresh() {
         cup.classList.remove('justify-flex-center')
         cup.classList.add('justify-flex-start');
         console.log("true-start");
+        cupJust = 1;
 
     } else if (inputJustify == "justify-content: flex-end;") {
         cup.classList.remove('justify-flex-start');
         cup.classList.remove('justify-flex-center');
         cup.classList.add('justify-flex-end');
         console.log("true-end");
+        cupJust = 2;
 
     } else if (inputJustify == "justify-content: center;") {
         cup.classList.remove('justify-flex-start');
         cup.classList.remove('justify-flex-end');
         cup.classList.add('justify-flex-center');
         console.log("true-center");
+        cupJust = 3;
 
     } else {
         cup.classList.remove('justify-flex-start');
@@ -89,23 +133,25 @@ function refresh() {
         cup.classList.remove('align-flex-center');
         cup.classList.add('align-flex-start');
         console.log("true-start")
+        cupAlign = 1;
 
     } else if (inputAlign == "align-items: flex-end;") {
         cup.classList.remove('align-flex-start');
         cup.classList.remove('align-flex-center');
         cup.classList.add('align-flex-end');
-        console.log("true-end")
+        console.log("true-end");
+        cupAlign = 2;
 
     } else if (inputAlign == "align-items: center;") {
         cup.classList.remove('align-flex-start');
         cup.classList.remove('align-flex-end');
         cup.classList.add('align-flex-center');
-        console.log("true-center")
+        console.log("true-center");
+        cupAlign = 3;
 
     } else {
         cup.classList.remove('align-flex-start');
         cup.classList.remove('align-flex-end');
         cup.classList.remove('align-flex-center');
     }
-
 }
